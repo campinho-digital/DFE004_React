29469900001b67af23392d203ed37dbf945bf618
Andrea dos Santos <canzua@icloud.com>
2023-07-13 15:12:56 -0300

    criacaoroutes-e-pages

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+Import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav id='navbar'>
+            <h2>
+                <Link to='/'>MovieMania</Link>
+            </h2>
+            <form>
+                <input type='text' placeholder='Busque um filme' />
+                <button type='submit'>Buscar</button>
+            </form>
+        </nav>
+    )
+};
+
+export default Navbar;
